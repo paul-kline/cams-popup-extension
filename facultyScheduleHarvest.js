30d4c2bb90d59f2cedb3f565653e05d989bb17fd
paul-kline <pauliankline@gmail.com>
2019-11-10 00:33:56 -0600

    added student functionality calendar export beta

--- facultyScheduleHarvest.js
+++ facultyScheduleHarvest.js
@@ -85,50 +85,6 @@ function mergeEvents(events) {
   });
   return result;
 }
-function adjustDate(date, days) {
-  //date: Date
-  //days : "MWF", etc.
-  //move the start date to the first day in list.
-  const daysLookup = ["U", "M", "T", "W", "R", "F", "S"];
-  days = days.split("");
-  const daysAsNumbers = days.map(d => daysLookup.indexOf(d));
-  if (daysAsNumbers.includes(date.getDay())) {
-    return date;
-  } else {
-    for (let i = 0; i < 7; i++) {
-      date.setDate(date.getDate() + 1);
-      if (daysAsNumbers.includes(date.getDay())) {
-        return date;
-      }
-    }
-  }
-  console.log("this should be impossible to reach. No future date was found in adjustDate", date, days);
-}
-function toDateTime(datestr, timestr) {
-  return setTime(toDate(datestr), timestr);
-}
-function toDate(str) {
-  const [_, month, day, year] = str.match(/(\d{1,2})\/(\d{1,2})\/(\d{4})/);
-  const d = new Date();
-  d.setFullYear(year);
-  d.setDate(day);
-  d.setMonth(month - 1);
-  return d;
-}
-function setTime(date, timestr) {
-  // "03:00 PM"
-  let [_, h, m] = timestr.match(/(\d+):(\d+)/);
-  h = Number.parseInt(h);
-  m = Number.parseInt(m);
-  if (timestr.toLowerCase().endsWith("pm")) {
-    h += 12;
-  }
-  date.setHours(h);
-  date.setMinutes(m);
-  date.setSeconds(0);
-  date.setMilliseconds(0);
-  return date;
-}
 
 /**
  * [{"course":"CS210A","title":"ESSENTIALS OF COMPUTING","startdate":"8/21/2019","enddate":"12/14/2019","schedule":[{"building":"Hudson","room":"HU-106","day":"M","starttime":"03:00 PM","endtime":"03:50 PM"},{"building":"Hudson","room":"HU-206","day":"W","starttime":"10:00 AM","endtime":"10:50 AM"}]}
@@ -175,246 +131,34 @@ function harvestSchedule() {
 }
 function placeButton() {
   const x = document.querySelector(".Page_Logo").nextElementSibling;
-  x.innerHTML =
-    x.innerHTML + "<button id='calendar' onclick='exportToGoogle()'> Export to my Google Calendar!</button>";
+  x.innerHTML = x.innerHTML + "<button id='calendar' onclick='callExport()'> Export to my Google Calendar!</button>";
 }
 placeButton();
+function callExport() {
+  console.log("boop. export clicked");
+  const sched = harvestSchedule();
+  const events = mergeEvents(sched.flatMap(toEvents));
+  console.log("schedule:", sched);
+  console.log("events:", events);
+  const p = document.getElementById("calendar").parentElement;
+  exportToGoogle(events, function(event) {
+    console.log("Events created for: " + event.htmlLink, event);
+    const elem = document.createElement("div");
+    elem.setAttribute("id", event.id);
+    elem.innerHTML =
+      "Events created for: " +
+      `<a href="${event.htmlLink}" target="_blank">${event.summary}</a><button onclick="deleteEvent('${event.id}')">delete event</button>`;
+    p.append(elem);
+  });
+}
 function addCode() {
   const elem = document.createElement("script");
-  let code = exportToGoogle.toString() + "\n";
   code += toEvents.toString() + "\n";
-  code += toDateTime.toString() + "\n";
-  code += toDate.toString() + "\n";
-  code += setTime.toString() + "\n";
   code += harvestSchedule.toString() + "\n";
-  code += adjustDate.toString() + "\n";
   code += mergeEvents.toString() + "\n";
-  code += deleteEvent.toString() + "\n";
 
   elem.innerHTML = code;
   document.querySelector("body").append(elem);
   addGoogleCode();
 }
 addCode();
-async function exportToGoogle() {
-  console.log("boop. export clicked");
-  const sched = harvestSchedule();
-  const events = mergeEvents(sched.flatMap(toEvents));
-  console.log("schedule:", sched);
-  console.log("events:", events);
-  await handleAuthClick();
-  // const batch = gapi.client.newBatch();
-  events.forEach(event => {
-    console.log("event submitting is: ", event);
-    const request = gapi.client.calendar.events.insert({
-      calendarId: "primary",
-      resource: event
-    });
-    const p = document.getElementById("calendar").parentElement;
-    const eventIDs = [];
-    request.execute(function(event) {
-      console.log("Events created for: " + event.htmlLink, event);
-      const elem = document.createElement("div");
-      elem.setAttribute("id", event.id);
-      elem.innerHTML =
-        "Events created for: " +
-        `<a href="${event.htmlLink}" target="_blank">${event.summary}</a><button onclick="deleteEvent('${event.id}')">delete event</button>`;
-      p.append(elem);
-      eventIDs.push(event.id);
-    });
-
-    // batch.add(request);
-  });
-  // batch.execute();
-  // window.batch = batch;
-  // console.log("batch:", batch);
-}
-
-// <!--Add buttons to initiate auth sequence and sign out-->
-// <button id="authorize_button" style="display: none;">Authorize</button>
-// <button id="signout_button" style="display: none;">Sign Out</button>
-
-function deleteEvent(eventId, callback) {
-  var params = {
-    calendarId: "primary",
-    eventId: eventId
-  };
-
-  const request = gapi.client.calendar.events.delete(params, function(err) {
-    if (err) {
-      console.log("The API returned an error: " + err);
-      return;
-    }
-    console.log("Event deleted.");
-    callback("Event deleted: " + eventId);
-  });
-  request.execute(event => {
-    console.log(event, "deleted");
-    document.getElementById(eventId).style.display = "none";
-  });
-}
-function googleCode() {
-  console.log("googleCode...");
-  // Client ID and API key from the Developer Console
-  window.CLIENT_ID = "820527871859-o85ov91ar8rrnos73i6qslvjov5361j1.apps.googleusercontent.com";
-  const clientsecret = "fedE3mPOcBfcLJmWkU2d-F0l";
-  window.API_KEY = apikey = "AIzaSyD-K091cR8wf1wJYfxfGFgBdfaUwm_TEr4";
-  // Array of API discovery doc URLs for APIs used by the quickstart
-  window.DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-  // Authorization scopes required by the API; multiple scopes can be
-  // included, separated by spaces.
-  window.SCOPES = "https://www.googleapis.com/auth/calendar";
-
-  //   var authorizeButton = document.getElementById('authorize_button');
-  //   var signoutButton = document.getElementById('signout_button');
-  /**
-   *  On load, called to load the auth2 library and API client library.
-   */
-  function handleClientLoad() {
-    console.log("handleClientLoad...");
-    gapi.load("client:auth2", initClient);
-  }
-  window.handleClientLoad = handleClientLoad;
-  /**
-   *  Initializes the API client library and sets up sign-in state
-   *  listeners.
-   */
-  function initClient() {
-    console.log("initClient...");
-    gapi.client
-      .init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-      })
-      .then(
-        function() {
-          // Listen for sign-in state changes.
-          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-          // Handle the initial sign-in state.
-          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-          //   authorizeButton.onclick = handleAuthClick;
-          //   signoutButton.onclick = handleSignoutClick;
-        },
-        function(error) {
-          appendPre(JSON.stringify(error, null, 2));
-        }
-      );
-  }
-  window.initClient = initClient;
-
-  /**
-   *  Called when the signed in status changes, to update the UI
-   *  appropriately. After a sign-in, the API is called.
-   */
-  function updateSigninStatus(isSignedIn) {
-    console.log("signed in!!!!", isSignedIn);
-    // if (isSignedIn) {
-    //   authorizeButton.style.display = 'none';
-    //   signoutButton.style.display = 'block';
-    //   listUpcomingEvents();
-    // } else {
-    //   authorizeButton.style.display = 'block';
-    //   signoutButton.style.display = 'none';
-    // }
-  }
-  window.updateSigninStatus = updateSigninStatus;
-  /**
-   *  Sign in the user upon button click.
-   */
-  async function handleAuthClick(
-    event,
-    f = () => {
-      console.log("signed in");
-    }
-  ) {
-    try {
-      await gapi.auth2.getAuthInstance().signIn(f);
-    } catch (e) {
-      console.log("failed to sign in:", e);
-    }
-    console.log("booooooop signed in finished!");
-  }
-  window.handleAuthClick = handleAuthClick;
-
-  /**
-   *  Sign out the user upon button click.
-   */
-  function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-  }
-  window.handleSignoutClick = handleSignoutClick;
-
-  /**
-   * Append a pre element to the body containing the given message
-   * as its text node. Used to display the results of the API call.
-   *
-   * @param {string} message Text to be placed in pre element.
-   */
-  function appendPre(message) {
-    // var pre = document.getElementById("content");
-    // var textContent = document.createTextNode(message + "\n");
-    // pre.appendChild(textContent);
-    alert(message);
-    console.log("from pre:", message);
-  }
-
-  /**
-   * Print the summary and start datetime/date of the next ten events in
-   * the authorized user's calendar. If no events are found an
-   * appropriate message is printed.
-   */
-  function listUpcomingEvents() {
-    gapi.client.calendar.events
-      .list({
-        calendarId: "primary",
-        timeMin: new Date().toISOString(),
-        showDeleted: false,
-        singleEvents: true,
-        maxResults: 10,
-        orderBy: "startTime"
-      })
-      .then(function(response) {
-        var events = response.result.items;
-        appendPre("Upcoming events:");
-
-        if (events.length > 0) {
-          for (i = 0; i < events.length; i++) {
-            var event = events[i];
-            var when = event.start.dateTime;
-            if (!when) {
-              when = event.start.date;
-            }
-            appendPre(event.summary + " (" + when + ")");
-          }
-        } else {
-          appendPre("No upcoming events found.");
-        }
-      });
-  }
-}
-function addGoogleCode() {
-  const googStarter = mkGscriptElem();
-  const body = document.querySelector("body");
-  body.append(googStarter);
-  const elem = document.createElement("script");
-  let content = googleCode.toString() + "; googleCode()";
-  elem.innerHTML = content;
-  body.append(elem);
-}
-function mkGscriptElem() {
-  const elem = document.createElement("script");
-  elem.setAttribute("async", "");
-  elem.setAttribute("defer", "");
-  //   elem.async = true;
-  //   elem.defer = true;
-  elem.src = "https://apis.google.com/js/api.js";
-  elem.setAttribute("onload", "this.onload=function(){};handleClientLoad()");
-  //   elem.onload = "this.onload=function(){};handleClientLoad()";
-  elem.setAttribute("onreadystatechange", "if (this.readyState === 'complete') this.onload()");
-  // elem.onreadystatechange = "if (this.readyState === 'complete') this.onload()";
-
-  return elem;
-}
---
+++ studentScheduleHarvest.js
@@ -0,0 +1,164 @@
+function toEvent(c) {
+  /* 
+course: "PH201"
+day: "MWF"
+endtime: "03:50 PM"
+instructor: "Luth, Karl"
+room: "ALUM-143"
+starttime: "03:00 PM"
+time: "03:00 PM - 03:50 PM"
+title: "GENERAL COLLEGE PHYSICS I"
+*/
+  const dayCode = {
+    U: "SU",
+    M: "MO",
+    T: "TU",
+    W: "WE",
+    R: "TH",
+    F: "FR",
+    S: "SA"
+  };
+  const event = {};
+  event.summary = c.course + " " + c.room;
+  event.room = c.room;
+  event.classTitle = c.course;
+  event.location = c.room;
+  event.description = c.title;
+  const start_dt = adjustDate(setTime(new Date(c.startdate), c.starttime), c.day);
+  const end_dt = adjustDate(setTime(new Date(c.startdate), c.endtime), c.day);
+  const timesuffix = "-0" + start_dt.getTimezoneOffset() / 60 + ":00";
+  event.start = {
+    dateTime: start_dt.toISOString(), //.replace(".000Z", "") + timesuffix,
+    timeZone: "America/Chicago"
+  };
+  event.end = {
+    dateTime: end_dt.toISOString(), //.replace(".000Z", "") + timesuffix,
+    timeZone: "America/Chicago"
+  };
+  if (c.enddate) {
+    const until_dt = toDateTime(c.enddate, c.endtime);
+    event.recurrence = [
+      `RRULE:FREQ=WEEKLY;UNTIL=${until_dt.toISOString().replace(/-|\.\d+|:/g, "")};WKST=SU;BYDAY=${c.day
+        .split("")
+        .map(x => dayCode[x])}`
+    ];
+  } else {
+    event.recurrence = [`RRULE:FREQ=WEEKLY;WKST=SU;BYDAY=${c.day.split("").map(x => dayCode[x])}`];
+  }
+
+  // event.recurrence = `RRULE:FREQ=WEEKLY;UNTIL=${until_dt.toISOString().replace(/-|\.\d+|:/g, "")}`;
+  event.attendees = [];
+  event.reminders = {
+    useDefault: true
+  };
+  return event;
+}
+function toEvents(classes) {
+  return classes.map(toEvent);
+}
+function parseDate(str) {
+  //yyyy-mm-dd
+  const d = new Date();
+  d.setMilliseconds(59);
+  d.setMinutes(59);
+  d.setHours(23);
+  [year, month, day] = str.split("-").map(Number.parseInt);
+  d.setFullYear(year);
+  d.setMonth(month - 1);
+  d.setDate(day);
+  return d;
+}
+function parseDateFromId(id) {
+  const e = document.getElementById(id);
+
+  if (e) {
+    return parseDate(e.value);
+  }
+}
+/**
+ * creates the array of objects from the webpage.
+ */
+function harvestStudentSchedule() {
+  const e = document.getElementById("startdate");
+  const startDate = e && e.value ? parseDate(e.value) : new Date();
+  const e2 = document.getElementById("enddate");
+  const endDate = e2 && e2.value ? parseDate(e2.value) : undefined;
+  const rows = document.querySelectorAll("table[summary='Course Schedule'] > tbody > tr");
+  const result = [];
+  //order of info:
+  //Dept	CrsID	Type	Section	CourseName	Instructor	Days	Room	Time	Date	Credits
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const cells = row.children;
+    const aclass = {};
+    aclass.startdate = startDate;
+    aclass.enddate = endDate;
+    aclass.course = cells[0].innerText + cells[1].innerText; //+ " " + cells[2].innerText;
+    if (!aclass.course.trim()) aclass.course = result[i - 1].course;
+    aclass.title = cells[4].innerText;
+    if (!aclass.title.trim()) aclass.title = result[i - 1].title;
+    aclass.instructor = cells[5].innerText;
+    if (!aclass.instructor.trim()) aclass.instructor = result[i - 1].instructor;
+    aclass.day = cells[6].innerText;
+    if (!aclass.day.trim()) aclass.day = result[i - 1].day;
+    aclass.room = cells[7].innerText;
+    if (!aclass.room.trim()) aclass.room = result[i - 1].day;
+    aclass.time = cells[8].innerText;
+    if (!aclass.time.trim()) aclass.time = result[i - 1].time;
+    //"starttime":"03:00 PM","endtime":"03:50 PM"
+    [aclass.starttime, aclass.endtime] = aclass.time.split(" - ");
+    result.push(aclass);
+  }
+  return result;
+}
+/**
+ *
+ * @param {HTMLInputElement} input to set
+ * @param {Date} date
+ */
+function setInputDate(input, date) {
+  input.value = date.getFullYear() + "-" + twof(date.getMonth() + 1) + "-" + twof(date.getDate());
+}
+function twof(x) {
+  x = "" + x;
+  return x.length < 2 ? "0" + x : x;
+}
+function placeButton() {
+  const x = document.querySelector(".Page_Logo").nextElementSibling;
+  x.innerHTML =
+    x.innerHTML +
+    "<button id='calendar' onclick='callExport()'> Export to my Google Calendar!</button><div>Semester start:<input type='date' id='startdate'></div><div>Semester end:<input type='date' id='enddate'></div>";
+}
+placeButton();
+function callExport() {
+  console.log("boop. export clicked");
+  const scheds = harvestStudentSchedule();
+  console.log("sched start times", scheds.map(s => s.starttime));
+  const events = toEvents(scheds);
+  console.log("events", events);
+  console.log("events", JSON.stringify(events));
+  //   console.log(scheds.map(s => s.startdate));
+  //   console.log(events.map(e => e.start));
+  const p = document.querySelector(".Page_Logo").nextElementSibling;
+  exportToGoogle(events, function(event) {
+    console.log("Events created for: " + event.htmlLink, event);
+
+    const str = `<div id='${event.id}'> Events created for: <a href="${event.htmlLink}" target="_blank">${event.summary}</a><button onclick="deleteEvent('${event.id}')">delete event</button></div>`;
+    p.innerHTML = p.innerHTML + str;
+  });
+}
+function addCode() {
+  const elem = document.createElement("script");
+  let code = toEvents.toString() + "\n";
+  code += toEvent.toString() + "\n";
+  code += harvestStudentSchedule.toString() + "\n";
+  code += callExport.toString() + "\n";
+  code += parseDate.toString() + "\n";
+  code += parseDateFromId.toString() + "\n";
+  code += setInputDate.toString() + "\n";
+  code += twof.toString() + "\n";
+
+  elem.innerHTML = code;
+  document.querySelector("body").append(elem);
+}
+addCode();
